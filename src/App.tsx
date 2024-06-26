49074fb973ef555994f8253e0c80500e58681da7
Alankar <alankar010499@gmail.com>
2024-06-26 20:17:16 +0530

    tic-tac-toe

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Block from "./components/Block";
 
 function App() {
+  const [state, setState] = useState(Array(9).fill(null));
+  const [currentTurn, setCurrentTurn] = useState("X");
+
+  const checkWinner = (state: any[]) => {
+    const win = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < win.length; i++) {
+      const [a, b, c] = win[i];
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c])
+        return true;
+    }
+    return false;
+  };
+
+  const handleOnClick = (index: number) => {
+    if (state[index]) return;
+    const stateCopy = Array.from(state);
+    stateCopy[index] = currentTurn;
+    const win = checkWinner(stateCopy);
+    
+    setCurrentTurn(currentTurn === "X" ? "O" : "X");
+
+    setState(stateCopy);
+    if (win) {
+      alert(`${currentTurn} won`);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="board">
+      <div className="row">
+        <Block onClick={() => handleOnClick(0)} value={state[0]} />
+        <Block value={state[1]} onClick={() => handleOnClick(1)} />
+        <Block value={state[2]} onClick={() => handleOnClick(2)} />
+      </div>
+
+      <div className="row">
+        <Block value={state[3]} onClick={() => handleOnClick(3)} />
+        <Block value={state[4]} onClick={() => handleOnClick(4)} />
+        <Block value={state[5]} onClick={() => handleOnClick(5)} />
+      </div>
+      <div className="row">
+        <Block value={state[6]} onClick={() => handleOnClick(6)} />
+        <Block value={state[7]} onClick={() => handleOnClick(7)} />
+        <Block value={state[8]} onClick={() => handleOnClick(8)} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Block.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface BlockProps {
+  value?: string | null;
+  onClick?: () => void;
+}
+
+const Block: React.FC<BlockProps> = ({ value , onClick }) => {
+  return <div onClick={onClick } className="block">{value}</div>;
+};
+
+export default Block;
